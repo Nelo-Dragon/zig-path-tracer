17a5d39c6f90ec8d6e0efe9b045330926826644b
null <null>
2026-09-25 10:03:33 -0400

    init objs added

--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,17 @@ const vk = @import("vulkan");
 
 pub fn main() !void {
 
+    var epd: vk.instance = undefined;
+
+    var ac: vk.AllowcationCallbacks = undefined;
+
+    var ici: vk.instanceCreateInfo = undefined;
+
+    vk.createInstance(ici, ac, epd);
+
     var gpu: vk.PhysicalDeviceProperties = undefined;
 
-    vk.GetPhysicalDeviceProperties(0, gpu);
+    vk.GetPhysicalDeviceProperties(epd, gpu);
 
     std.debug.print(gpu.deviceName[_], .{});
 }
